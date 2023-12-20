4c6728c64f09e4d6a6bef149ad356808cc5d2d57
masoopust <147604966+masoopust@users.noreply.github.com>
2023-12-20 10:35:53 +0100

    Update generator.py

--- generator.py
+++ generator.py
@@ -3,11 +3,12 @@ import random
 Loremtext = ["Accio", "Aquamenti", "Alohomora", "Ascendio", "Avada Kedavra", "Bombarda","Cruciatus", "Descendo", "Erecto", "Expelliarmus","Geminio","Hop", "Imperius", "Lumos", "Obscuro", "Perfendi",
              "Reducto", "Silencio", "Titillando", "Verdillious", "Wingardium Leviosa", "Zapomeň", "Žer slimáky"   ]
 
-numbsent = int(input("How many sentences you want from generator? \n: "))           #number of sentences
-
+numbsent = int(input("How many sentences you want from generator? \n: "))    #number of sentences
+wsmin = int(input("What is the minimum of words you want in one sentence? \n: "))   #Minimum words
+wsmax = int(input("What is the maximum of words you want in one sentence? \n: "))   #Maximum words
 text = []
 for i in range(numbsent):
-    ws = random.randint(3,8)                                                         #words in sentence
+    ws = random.randint(wsmin,wsmax)                                                         #words in sentence
     sentence = " ".join(random.sample(Loremtext, ws))                                #sentence = náhodný počet slov 3-8 ze seznamu Loremtext a dá je do stringu
     text.append(sentence.capitalize() + ".")                                         #První písmeno udělá velké, na konec přidá tečku a přídá do seznamu text
 
